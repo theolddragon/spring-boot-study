e212f60ff047cc2fd5a906169428e7154fb80db6
Park Dongwook <wanderhoward@gmail.com>
2018-08-26 18:34:08 +0900

    백기선의 스프링 부트 개념과 활용
     - 3장 내장 웹 서버 응용 2부: HTTPS와 HTTP2
       - HTTP2 설정

--- src/main/java/kr/theolddragon/springBootGettingStarted/Application.java
+++ src/main/java/kr/theolddragon/springBootGettingStarted/Application.java
@@ -1,11 +1,7 @@
 package kr.theolddragon.springBootGettingStarted;
 
-import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,19 +16,6 @@ public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
-  }
-
-  @Bean
-  public ServletWebServerFactory servletContainer() {
-    TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-    tomcat.addAdditionalTomcatConnectors(createStandardConnector());
-    return tomcat;
-  }
-
-  private Connector createStandardConnector() {
-    Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-    connector.setPort(18080);
-    return connector;
   }
 
 }
